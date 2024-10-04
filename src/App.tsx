1c0e7aac8f1efcd9d6920ffd1ad826f174e85661
seulgi chu <seulgichu14@gmail.com>
2024-10-05 01:24:51 +0900

    Fix: 무한 스크롤 에러 해결 및 스켈레톤 컴포넌트 연결

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import useIntersect from "./hooks/useIntersect";
 import calculateAllPrices from "./utils/calculateAllPrices";
 import getMockData from "./utils/getMockData";
 import { IProduct } from "./types/MockData";
+import Skeleton from "./components/Skeleton";
 
 function App() {
   const [products, setProducts] = useState<IProduct[]>([]);
@@ -13,9 +14,12 @@ function App() {
   const [pageNumber, setPageNumber] = useState<number>(1);
   const [isLoaded, setIsLoaded] = useState<boolean>(false);
   const [isEnded, setIsEnded] = useState<boolean>(false);
+  const [isFetching, setIsFetching] = useState<boolean>(false);
 
   useEffect(() => {
     async function fetchProducts(page: number) {
+      setIsFetching(true);
+
       const { datas, isEnd } = await getMockData(page);
       const sum = calculateAllPrices(datas);
 
@@ -24,6 +28,8 @@ function App() {
 
       setIsEnded(isEnd);
       setIsLoaded(true);
+
+      setIsFetching(false);
     }
 
     if (!isEnded) {
@@ -34,14 +40,10 @@ function App() {
   const ref = useIntersect(async (entry, observer) => {
     observer.unobserve(entry.target);
 
-    if (!isEnded) {
+    if (!isEnded && !isFetching) {
       setPageNumber((prevNumber) => prevNumber + 1);
     }
   });
-
-  if (!isLoaded) {
-    return <div>Loading...</div>;
-  }
 
   return (
     <div className="p-12 h-full min-h-screen bg-slate-200">
@@ -49,8 +51,13 @@ function App() {
         <h1 className="mb-6 text-2xl font-bold">전체 상품</h1>
         <p className="text-xl font-base font-semibold text-blue-600">$ {allPrices}</p>
       </div>
-      {products && products.map((product) => <Card product={product} key={product.productId} />)}
+      {!isLoaded
+        ? Array(6)
+            .fill(0)
+            .map((_, index) => <Skeleton key={index} />)
+        : products.map((product) => <Card product={product} key={product.productId} />)}
       <div ref={ref} className="loading-indicator">
+        {isFetching && <Skeleton />}
         {!isEnded ? "Loading more..." : "No more products"}
       </div>
     </div>
