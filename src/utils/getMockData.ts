80932ac85aa649ca42c32c9eb63448eb9c0f2c94
seulgi chu <seulgichu14@gmail.com>
2024-10-04 13:21:30 +0900

    Feat: 목데이터 호출 getMockData 함수 구현

---
+++ src/utils/getMockData.ts
@@ -0,0 +1,18 @@
+import { IProduct } from "../types/MockData";
+import NUMBER from "../constants/number";
+import MOCK_DATA from "../mocks/mockData";
+
+const { PER_PAGE, TIME_RANGE } = NUMBER;
+
+function getMockData(pageNumber: number) {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      const datas: IProduct[] = MOCK_DATA.slice(PER_PAGE * pageNumber, PER_PAGE * (pageNumber + 1));
+      const isEnd = PER_PAGE * (pageNumber + 1) >= MOCK_DATA.length;
+
+      resolve({ datas, isEnd });
+    }, TIME_RANGE);
+  });
+}
+
+export default getMockData;
